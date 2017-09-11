a0625b0c999615a9df15256b7961a2d0ca33305d
1185432424@qq.com <yx15797180568>
2017-09-11 09:47:59 +0800

    dao方法

--- src/main/java/com/item/finance/dao/Yx_FinanceProductFunds_Dao.java
+++ src/main/java/com/item/finance/dao/Yx_FinanceProductFunds_Dao.java
@@ -1,5 +1,7 @@
 package com.item.finance.dao;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +21,23 @@ public class Yx_FinanceProductFunds_Dao {
 		Session session=getSession();
 		session.save(financeProductFunds);
 	}
-
+	public FinanceProductFunds selectFinanceProductFunds(String id){
+		Session session=getSession();
+		FinanceProductFunds financeProductFunds=(FinanceProductFunds)session.get(FinanceProductFunds.class, id);
+	return financeProductFunds;
+	}
+	public List<FinanceProductFunds> listFinanceProductFunds(){
+		Session session=getSession();
+		String hql="from FinanceProductFunds";
+		List<FinanceProductFunds> list=session.createQuery(hql).list();
+		return list;
+	}
+    public void deleteFinanceProductFunds(FinanceProductFunds financeProductFunds){
+    	Session session=getSession();
+    	session.delete(financeProductFunds);
+    }
+    public void updateFinanceProductFunds(FinanceProductFunds financeProductFunds){
+    	Session session=getSession();
+    	session.update(financeProductFunds);
+    }
 }
