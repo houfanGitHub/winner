825bf110217c5de1029acd081ffc971744c812ef
sushuang <sushuang402@163.com>
2017-09-11 17:47:32 +0800

    账号管理service

---
+++ src/main/java/com/item/finance/services/Ss_zhmanagerService.java
@@ -0,0 +1,59 @@
+package com.item.finance.services;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.loader.custom.Return;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.item.finance.bean.Member;
+import com.item.finance.dao.Ss_zhManangerDao;
+
+@Service
+@Transactional
+public class Ss_zhmanagerService implements ItemServiceImpl<Member>  {
+
+	
+	@Autowired
+	private Ss_zhManangerDao Ss_zhManangerDao;
+
+	@Override
+	public Member selectGetById(String id) {
+		Member member=Ss_zhManangerDao.getId(id);
+		return member;
+	}
+
+	@Override
+	public List<Member> list() {
+		
+		return null;
+	}
+	//显示
+public List<Member> list(Map map) {
+	return this.Ss_zhManangerDao.listzh(map);
+
+	}
+	
+
+	@Override
+	public void save(Member t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Member t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Member t) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
