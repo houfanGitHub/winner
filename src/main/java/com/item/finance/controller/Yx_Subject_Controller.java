7f8ce1c131396c456eae5b5d470f845bd3f11f3a
1185432424@qq.com <yx15797180568>
2017-09-11 14:41:29 +0800

    添加controler方法

--- src/main/java/com/item/finance/controller/Yx_Subject_Controller.java
+++ src/main/java/com/item/finance/controller/Yx_Subject_Controller.java
@@ -4,18 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.item.finance.bean.FinanceProductFunds;
+import com.item.finance.bean.Subject;
+import com.item.finance.services.Yx_Subject_Services;
+
+
 
 @Controller
-@RequestMapping("/Yx")
+@RequestMapping("/yx")
 public class Yx_Subject_Controller {
     @Autowired
-	private Yx_Subject_Controller yx_FinanceProductFunds_Services;
+	private Yx_Subject_Services yx_FinanceProductFunds_Services;
+    
     
     @RequestMapping("/list")
-    public String list(){
-    	return null;
+    public String list(Model model,Subject subject){
+    	System.out.println(subject);
+    List<Subject> list=yx_FinanceProductFunds_Services.list();
+    model.addAttribute("list", list);
+    	return "/WEB-INF/yx_subject";
     }
+
+
+
 }
